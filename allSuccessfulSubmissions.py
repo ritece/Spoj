b985d605df5fb8ab12f30d0d17fdee0060bb4544
ritece <riteshpratapsingh94@gmail.com>
2016-03-14 00:50:54 +0530

    Fetch all the successfully solved problems of a user on spoj.com

---
+++ allSuccessfulSubmissions.py
@@ -0,0 +1,28 @@
+import urllib2
+from bs4 import BeautifulSoup 
+import re
+
+'''
+This program will ask for the spoj username and then fetch the content from the 
+url - http://www.spoj.com/status/userName/all
+
+'''
+
+userName = raw_input('Enter the spoj username for the user you want to fetch the submissions : ')
+
+spojUrl = 'http://www.spoj.com/users/'+userName
+
+
+x = urllib2.urlopen(spojUrl)
+html = x.read()
+soup = BeautifulSoup(html)
+listOfSuccessfulSubmission = soup.find_all('table', class_='table table-condensed')[0].find_all('td')
+listOfSolvedProblemCodes = []
+for submission in listOfSuccessfulSubmission:
+	problemCode = submission.get_text()
+	if problemCode:
+		listOfSolvedProblemCodes.append(problemCode)
+
+print userName+ ' has solved',len(listOfSolvedProblemCodes),'problems on Spoj.'
+
+
